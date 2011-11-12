8db61fe09dec5c10ca5e5dfe703953a9d225b7fb
Tobias Bielohlawek <tobi@soundcloud.com>
2011-11-12 15:37:09 +0100

    buffered version

--- Datalogger.ino
+++ Datalogger.ino
@@ -2,7 +2,7 @@
 #include <SD.h>
 #include <TimerOne.h>
 #include <ByteBuffer.h>
-#include <EEPROM.h>
+// #include <EEPROM.h>
 
 // #define MEGA_SOFT_SPI 1
 
@@ -10,34 +10,41 @@
 #define READ_CHAN2 PINB & 0x06 //pins 9, 10
 #define READ_CHAN3 PINC & 0x03 //pins 9, 10
 
-#define SEND_BUFFER_SIZE 1000
-#define MAX_COUNT 100000  //4,294,967,295
+#define SEND_BUFFER_SIZE 10
+
+#define MAX_COUNT 200000 //number of Bytes to write MAX: 2M
 
 #define EEPROM_ADR 0
 
-unsigned long cnt = 0;
+long cnt = 0;
+long input = 0;
 
-File file;
+File outFile;
 ByteBuffer buffer;
 
 char filename[] = "turtleX.log";
 
 int ledState = LOW;
 
-void measure() {
-  byte data = READ_CHAN1;
-  byte port = READ_CHAN3;
-  buffer.put(data);
-  buffer.put(port);
-}
-
 void toggle() {
-  ledState = (ledState == LOW) ? HIGH : LOW;
-  digitalWrite(A5, ledState);
+  digitalWrite(A5, HIGH);
+  delay(100);
+  digitalWrite(A5, LOW);
   delay(100);
 }
 
+void measure() {
+  buffer[pointer++] = READ_CHAN1;
+  buffer[pointer++] = READ_CHAN3;
+  
+  input += 2;
+  //buffer.put(data);
+  //buffer.put(port);
+}
+
 void setup() {
+  //EEPROM.write(EEPROM_ADR, 0);
+
   DDRD = 0; //init pins as INPUT
   //pinMode(9, INPUT);
   //pinMode(10, INPUT);
@@ -50,9 +57,9 @@ void setup() {
 
   toggle();
 
-  int value = EEPROM.read(EEPROM_ADR);
-  filename[6] = value + 48; //add 48 to make it ASCII
-  EEPROM.write(EEPROM_ADR, ++value);
+  //int value = EEPROM.read(EEPROM_ADR);
+  filename[6] = '0'; //value + 48; //add 48 to make it ASCII
+  //EEPROM.write(EEPROM_ADR, ++value);
 
   toggle();
 
@@ -64,27 +71,42 @@ void setup() {
 
   toggle();
 
-  file = SD.open(filename, FILE_WRITE);
+  if(SD.exists(filename)) {
+    SD.remove(filename);
+  }
+  outFile = SD.open(filename, O_WRITE | O_CREAT);
 
   toggle();
 
   //prepare Timer
-  Timer1.initialize(20); // 0.02us period = 50kHz
+  //Timer1.initialize(20); // microseconds -> 0.02ms period -> 50kHz
+  Timer1.initialize(50); // microseconds -> 0.02ms period -> 50kHz
   Timer1.attachInterrupt(measure);
-
-  digitalWrite(A5, LOW);
 }
+
+#define WRITE_FLUSH 5
 
 void loop()
 {
   if(cnt >= MAX_COUNT ) {
-    file.close();
     digitalWrite(A5, HIGH);
+    outFile.close();
   }
-  else if(buffer.getSize() > 0) {
+  else if(input >= WRITE_FLUSH) {
+    byte array[WRITE_FLUSH];
+    for(int k = 0; k < WRITE_FLUSH; k++) {
+      //array[k] = buffer.get();
+      input --;
+    }
+    outFile.write(array, WRITE_FLUSH);
+    //outFile.write(1);
+    cnt += WRITE_FLUSH;
+  }
+  /*
+  else if(buffer.getSize() >= 1) {
     byte data = buffer.get();
-    file.write(data);
-    cnt++;
-  }
+    outFile.write(data);
+    cnt += 1;
+  }*/
 }
 
