38d334521e00376100c3e7fc6a3896128f924eb5
Tobias Bielohlawek <tobi@soundcloud.com>
2011-12-27 23:05:03 +0100

    working version

--- dnorm.rb
+++ dnorm.rb
@@ -8,22 +8,29 @@ class Array
   end
 end
 
-file = "TURTLEC.LOG"
+file = "TB.LOG"
 
 def dnorm(file)
   content = open(file, "rb") {|io| io.read }
   anz = 0
-  oline = nil
-  odata = nil
-  File.open("d3_#{file}", 'w') do |f|
+  oline = []
+  File.open("d1_#{file}", 'w') do |f|
     (content.size / 2).times do |cnt|
-      data = content[cnt*2+1]
-      clk  = (content[cnt*2] & 0x01)
-      st   = (content[cnt*2] & 0x02) >> 1
-      line = [data, clk, st].join(",")
+      data  = content[cnt*2+1]
+      reset = (content[cnt*2] & 0x01)
+      clk   = (content[cnt*2] & 0x02) >> 1
+      st    = (content[cnt*2] & 0x10) >> 4
+      busy  = (content[cnt*2] & 0x20) >> 5
+      line = [reset, clk, st, data]
       if line != oline
-        f.write("#{line},#{anz}\n")# if st > 0# && (data != odata)
-        odata = (clk == 0) ? nil : data
+       # print ",#{anz}\n"
+        puts line.join(" - ") if st == 1 && clk == 1
+        puts if st == 0
+        #
+        #   f.write(",#{anz}\n")
+        #   f.write("\n") if line.first != oline.first
+        #   f.write(line.join(","))
+        # end
         oline = line
         anz = 0
       else
@@ -32,6 +39,70 @@ def dnorm(file)
     end
   end
 end
+
+def dnorm2(file)
+  content = open(file, "rb") {|io| io.read }
+  anz = 0
+  oline = nil
+  odata = nil
+  File.open("d2_#{file}", 'w') do |f|
+    (content.size / 2).times do |cnt|
+      data = content[cnt*2+1]
+      clk  = (content[cnt*2] & 0x01)
+      st   = (content[cnt*2] & 0x02) >> 1
+      f.write("#{data}\n") if odata != data
+      data = nil if odata == data && clk == 0 && st == 0
+      odata = data
+    end
+  end
+end
+
+
+@length = 0
+@d = {}
+
+
+def add(data, cnt)
+  if @length == 0
+    @length = data
+  else
+    @d[cnt] = data
+    @length -= 1
+  end
+  if @length == 0
+    keys = @d.keys
+    puts "#{keys.min}-#{keys.max}: #{@d.values.join(' ')}"
+    @d = {}
+  end
+end
+
+def cluster(file)
+  content = open(file, "rb") {|io| io.read }
+  anz = 0
+  a = 0
+  last = nil
+  File.open("cluster_#{file}", 'w') do |f|
+    (content.size / 2).times do |cnt|
+      data = content[cnt*2+1]
+      clk  = (content[cnt*2] & 0x01)
+      st   = (content[cnt*2] & 0x02) >> 1
+
+      if last != data && (clk == 1 || st == 1)
+        last = data
+        f.write(",#{anz}\n#{data}")
+        add(data,cnt)
+        anz = 0
+        a += 1;
+      else
+        anz += 1
+      end
+      if st == 0
+        last = nil
+      end
+    end
+  end
+end
+
 
 dnorm(file)
 
